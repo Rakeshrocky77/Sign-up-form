d00531322fadb6effffbd572ee5dfd4e1b09963a
Rakesh <rakesh.h7997@gmail.com>
2023-10-21 14:08:34 +0530

    compelte project

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function {
+document.addEventListener("DOMContentLoaded", function() {
     let password = document.querySelector("#password").value;
     let confirmPassword = document.querySelector("#confirmPassword").value;
 
     let error = document.querySelector("#error");
        
-    if(password = "" && confirmPassword == ""){
+    if(password == "" && confirmPassword == ""){
         error.innerHTML = "*passwords do not match";
     }
     else if (password === confirmPassword){
